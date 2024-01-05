1058c40c9fb5d8eb8226f95e0a464445ee4a19d2
minhyeongKang <kang7937@gmail.com>
2024-01-05 17:45:23 +0900

    feat:  Comment Entity 생성 및 패키지 이동

---
+++ src/main/java/com/wemakeplay/wemakeplay/domain/comment/entity/Comment.java
@@ -0,0 +1,51 @@
+package com.wemakeplay.wemakeplay.domain.comment.entity;
+
+import com.wemakeplay.wemakeplay.domain.board.entity.Board;
+import com.wemakeplay.wemakeplay.domain.user.entity.User;
+import com.wemakeplay.wemakeplay.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Board board;
+
+    @Builder
+    public Comment(String content, User user, Board board) {
+        this.content = content;
+        this.user = user;
+        this.board = board;
+    }
+
+}
