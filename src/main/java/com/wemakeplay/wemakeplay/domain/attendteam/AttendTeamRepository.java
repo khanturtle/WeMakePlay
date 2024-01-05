331a7a668cb5eb472ba52f5b516e33e2aca04388
k <kkkjjj320320@naver.com>
2024-01-06 05:49:33 +0900

    feat: AttendTeamRepository 인터페이스 추가

---
+++ src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeamRepository.java
@@ -0,0 +1,8 @@
+package com.wemakeplay.wemakeplay.domain.attendteam;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttendTeamRepository extends JpaRepository<AttendTeam, Long> {
+    List<AttendTeam> findByTeamId(Long teamId);
+}
