40615ac0e61176e41dd33f36cc2b5686e47f0274
k <kkkjjj320320@naver.com>
2024-01-06 05:56:32 +0900

    feat: Team Entity 추가

---
+++ src/main/java/com/wemakeplay/wemakeplay/domain/team/entity/Team.java
@@ -0,0 +1,47 @@
+package com.wemakeplay.wemakeplay.domain.team.entity;
+
+import com.wemakeplay.wemakeplay.domain.attendteam.AttendTeam;
+import com.wemakeplay.wemakeplay.domain.team.dto.TeamRequestDto;
+import com.wemakeplay.wemakeplay.domain.user.entity.User;
+import com.wemakeplay.wemakeplay.global.security.UserDetailsImpl;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "teams")
+@NoArgsConstructor
+@Getter
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String teamName;
+    @Column(nullable = false, unique = true)
+    private String teamIntro;
+
+    // 생성지
+    @ManyToOne
+    private User teamOwner;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<AttendTeam> attendTeams = new ArrayList<>();
+
+    public Team(TeamRequestDto teamRequestDto, UserDetailsImpl userDetails){
+        this.teamName = teamRequestDto.getTeamName();
+        this.teamIntro = teamRequestDto.getTeamIntro();
+        this.teamOwner = userDetails.getUser();
+    }
+
+    public void updateTeam(TeamRequestDto teamRequestDto){
+        this.teamName = teamRequestDto.getTeamName();
+        this.teamIntro = teamRequestDto.getTeamIntro();
+    }
+}
