2c71a1d273e32fb6d7a38f9f4ee436ddeb6670a2
minhyeongKang <kang7937@gmail.com>
2024-01-05 17:46:56 +0900

    feat:  CommentRes 생성

---
+++ src/main/java/com/wemakeplay/wemakeplay/domain/comment/dto/response/CommentResponseDto.java
@@ -0,0 +1,23 @@
+package com.wemakeplay.wemakeplay.domain.comment.dto.response;
+
+import com.wemakeplay.wemakeplay.domain.comment.entity.Comment;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class CommentResponseDto {
+
+    private Long id;
+    private String nickname;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.nickname = comment.getUser().getNickname();
+        this.content = comment.getContent();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+    }
+}
