d6287eb5e5ab802b3a48dbaa709f9a99682eb264
minhyeongKang <kang7937@gmail.com>
2024-01-05 18:55:54 +0900

    feat: CommentService 댓글 수정 기능 구현

--- src/main/java/com/wemakeplay/wemakeplay/domain/comment/service/CommentService.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/comment/service/CommentService.java
@@ -1,7 +1,9 @@
 package com.wemakeplay.wemakeplay.domain.comment.service;
 
 import static com.wemakeplay.wemakeplay.global.exception.ErrorCode.NOT_COMMENT_CONTENT;
+import static com.wemakeplay.wemakeplay.global.exception.ErrorCode.NOT_COMMENT_OWNER;
 import static com.wemakeplay.wemakeplay.global.exception.ErrorCode.NOT_EXIST_BOARD;
+import static com.wemakeplay.wemakeplay.global.exception.ErrorCode.NOT_EXIST_COMMENT;
 
 import com.wemakeplay.wemakeplay.domain.board.entity.Board;
 import com.wemakeplay.wemakeplay.domain.board.repository.BoardRepository;
@@ -21,7 +23,8 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final BoardRepository boardRepository;
 
-    public CommentResponseDto createComment(User user, Long boardId, CommentRequestDto commentRequestDto) {
+    public CommentResponseDto createComment(
+        User user, Long boardId, CommentRequestDto commentRequestDto) {
         if (commentRequestDto.getContent() == null) {
             throw new ServiceException(NOT_COMMENT_CONTENT);
         }
@@ -34,8 +37,24 @@ public class CommentService {
             .user(user)
             .board(board)
             .build();
+
         commentRepository.save(comment);
 
         return new  CommentResponseDto(comment);
+    }
+
+    public CommentResponseDto updateComment(
+        User user, Long commentId, CommentRequestDto commentRequestDto) {
+
+        Comment comment = commentRepository.findById(commentId)
+            .orElseThrow(() -> new ServiceException(NOT_EXIST_COMMENT));
+
+        if (!user.getId().equals(comment.getUser().getId())) {
+            throw new ServiceException(NOT_COMMENT_OWNER);
+        }
+
+        comment.update(commentRequestDto);
+
+        return new CommentResponseDto(comment);
     }
 }
