9104800f8ac7fe18c491c8a7576666b6a51d7a7f
khanturtle <chojunho0310@naver.com>
2024-01-05 11:40:19 +0900

    git init

---
+++ src/main/java/com/wemakeplay/wemakeplay/WemakeplayApplication.java
@@ -0,0 +1,13 @@
+package com.wemakeplay.wemakeplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemakeplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemakeplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wemakeplay/wemakeplay/WemakeplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemakeplay.wemakeplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemakeplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
