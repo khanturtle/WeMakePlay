015fac07b26f8fefa3301ad908b806226d26ebad
minhyeongKang <kang7937@gmail.com>
2024-01-08 21:05:17 +0900

    feat: FollowController 팔로잉 취소 기능 구현

--- src/main/java/com/wemakeplay/wemakeplay/domain/follow/controller/FollowController.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/follow/controller/FollowController.java
@@ -2,11 +2,13 @@ package com.wemakeplay.wemakeplay.domain.follow.controller;
 
 import com.wemakeplay.wemakeplay.domain.follow.dto.FollowResponseDto;
 import com.wemakeplay.wemakeplay.domain.follow.service.FollowService;
+import com.wemakeplay.wemakeplay.domain.like.dto.LikeResponseDto;
 import com.wemakeplay.wemakeplay.global.dto.RootResponseDto;
 import com.wemakeplay.wemakeplay.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,21 @@ public class FollowController {
         return ResponseEntity.ok(RootResponseDto.builder()
             .code("201")
             .message(userId + "번 유저 팔로우 신청 완료")
+            .data(responseDto)
+            .build()
+        );
+    }
+
+    @DeleteMapping("{userId}")
+    public ResponseEntity<?> unPressLike(
+        @PathVariable(name = "userId") Long userId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        FollowResponseDto responseDto = followService.unFollowUser(userId, userDetails.getUser());
+
+        return ResponseEntity.ok(RootResponseDto.builder()
+            .code("200")
+            .message(userId + "번 유저 팔로우 취소")
             .data(responseDto)
             .build()
         );
