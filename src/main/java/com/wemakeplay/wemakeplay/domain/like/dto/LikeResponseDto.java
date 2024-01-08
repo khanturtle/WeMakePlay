e2d6fd4c77cdc6951393464dbbc583fba13d6eeb
minhyeongKang <kang7937@gmail.com>
2024-01-08 17:20:03 +0900

    feat: LikeResponseDto 좋아요 생성 삭제 계산 메서드 추가

--- src/main/java/com/wemakeplay/wemakeplay/domain/like/dto/LikeResponseDto.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/like/dto/LikeResponseDto.java
@@ -11,10 +11,16 @@ public class LikeResponseDto {
     private Long userId;
     private String username;
     private String nickname;
+    private Long likeCount;
 
-    public LikeResponseDto(Like like) {
+    public LikeResponseDto(Like like, Long likeCount) {
         this.userId = like.getLikeUser().getId();
         this.username = like.getLikeUser().getUsername();
         this.nickname = like.getLikeUser().getNickname();
+        this.likeCount = likeCount;
+    }
+
+    public void calculateLikeCount(Long count) {
+        this.likeCount = count;
     }
 }
