959178a83f40995b5230a9d207983895d830e859
minhyeongKang <kang7937@gmail.com>
2024-01-08 15:28:57 +0900

    feat: LikeController 좋아요 생성 기능 구현

---
+++ src/main/java/com/wemakeplay/wemakeplay/domain/like/controller/LikeController.java
@@ -0,0 +1,39 @@
+package com.wemakeplay.wemakeplay.domain.like.controller;
+
+import com.wemakeplay.wemakeplay.domain.like.dto.LikeResponseDto;
+import com.wemakeplay.wemakeplay.domain.like.service.LikeService;
+import com.wemakeplay.wemakeplay.global.dto.RootResponseDto;
+import com.wemakeplay.wemakeplay.global.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/likes")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<?> pressLike(
+        @PathVariable(name = "userId") Long userId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+
+        LikeResponseDto responseDto = likeService.pressLike(userId, userDetails.getUser());
+
+        return ResponseEntity.ok(RootResponseDto.builder()
+                .code("201")
+                .message(userId + "번 유저 좋아요")
+                .data(responseDto)
+            .build()
+        );
+
+    }
+}
