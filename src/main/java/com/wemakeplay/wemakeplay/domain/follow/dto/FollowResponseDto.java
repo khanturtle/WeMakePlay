45c09b74cdc84b4770389f4a591978051dc1a863
minhyeongKang <kang7937@gmail.com>
2024-01-08 20:45:03 +0900

    feat: FollowResponseDto 생성

---
+++ src/main/java/com/wemakeplay/wemakeplay/domain/follow/dto/FollowResponseDto.java
@@ -0,0 +1,20 @@
+package com.wemakeplay.wemakeplay.domain.follow.dto;
+
+import com.wemakeplay.wemakeplay.domain.follow.entity.Follow;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FollowResponseDto {
+
+    private Long userId;
+    private String username;
+    private String nickname;
+
+    public FollowResponseDto(Follow follow) {
+        this.userId = follow.getId();
+        this.username = follow.getFollowingUser().getUsername();
+        this.nickname = follow.getFollowingUser().getNickname();
+    }
+}
