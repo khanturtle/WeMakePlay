d8bcb984b24a712f9a321341d4728b67651f8c1c
minhyeongKang <kang7937@gmail.com>
2024-01-08 21:05:52 +0900

    feat: FollowRepository 쿼리문 추가

--- src/main/java/com/wemakeplay/wemakeplay/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/follow/repository/FollowRepository.java
@@ -2,9 +2,12 @@ package com.wemakeplay.wemakeplay.domain.follow.repository;
 
 import com.wemakeplay.wemakeplay.domain.follow.entity.Follow;
 import com.wemakeplay.wemakeplay.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByUserAndFollowingUser(User user, User followingUser);
+
+    Optional<Follow> findByUserAndFollowingUser(User user, User followingUser);
 }
