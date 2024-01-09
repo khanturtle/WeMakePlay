cd1d7a5fe30800b0a8915390638076ff2ecc11a1
minhyeongKang <kang7937@gmail.com>
2024-01-09 15:14:26 +0900

    feat: BoardRepository 타인 조회 기능 구현 관련 쿼리메서드 추가

--- src/main/java/com/wemakeplay/wemakeplay/domain/board/repository/BoardRepository.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/board/repository/BoardRepository.java
@@ -1,7 +1,10 @@
 package com.wemakeplay.wemakeplay.domain.board.repository;
 
 import com.wemakeplay.wemakeplay.domain.board.entity.Board;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
+
+    List<Board> findByBoardOwnerId(Long id);
 }
