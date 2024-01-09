15d9ca4a3f85bed05f70b59cb27393ee2ec9a53a
k <kkkjjj320320@naver.com>
2024-01-09 16:41:53 +0900

    docs: 문서 정리

--- src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
@@ -14,19 +14,25 @@ public class AttendTeam {
     @Id
     @GeneratedValue
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
-    @Enumerated
+
+    @Enumerated(EnumType.STRING)
     private Participation participation;
 
     public AttendTeam(Team team, User user, Participation participation){
         this.user = user;
         this.team = team;
         this.participation = participation;
+    }
+
+    public AttendTeam(Team team, User user, com.wemakeplay.wemakeplay.domain.attendboard.Participation participation) {
     }
 
     public void allowAttend(){
