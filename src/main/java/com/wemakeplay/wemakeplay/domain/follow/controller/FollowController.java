be6bb0e1463d310d2e28a7fcfa832fbc3df3eccc
minhyeongKang <kang7937@gmail.com>
2024-01-09 11:59:36 +0900

    feat: FollowController 팔로잉 팔러워 조회 기능 구현

--- src/main/java/com/wemakeplay/wemakeplay/domain/follow/controller/FollowController.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/follow/controller/FollowController.java
@@ -1,13 +1,16 @@
 package com.wemakeplay.wemakeplay.domain.follow.controller;
 
-import com.wemakeplay.wemakeplay.domain.follow.dto.FollowResponseDto;
+import com.wemakeplay.wemakeplay.domain.follow.dto.FollowerResponseDto;
+import com.wemakeplay.wemakeplay.domain.follow.dto.FollowingResponseDto;
 import com.wemakeplay.wemakeplay.domain.follow.service.FollowService;
 import com.wemakeplay.wemakeplay.global.dto.RootResponseDto;
 import com.wemakeplay.wemakeplay.global.security.UserDetailsImpl;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +29,7 @@ public class FollowController {
         @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
 
-        FollowResponseDto responseDto = followService.followUser(userId, userDetails.getUser());
+        FollowingResponseDto responseDto = followService.followUser(userId, userDetails.getUser());
 
         return ResponseEntity.ok(RootResponseDto.builder()
             .code("201")
@@ -41,11 +44,42 @@ public class FollowController {
         @PathVariable(name = "userId") Long userId,
         @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        FollowResponseDto responseDto = followService.unFollowUser(userId, userDetails.getUser());
+
+        FollowingResponseDto responseDto = followService.unFollowUser(userId, userDetails.getUser());
 
         return ResponseEntity.ok(RootResponseDto.builder()
             .code("200")
             .message(userId + "번 유저 팔로우 취소")
+            .data(responseDto)
+            .build()
+        );
+    }
+
+    @GetMapping("/following")
+    public ResponseEntity<?> getFollowing(
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+
+        List<FollowingResponseDto> responseDto = followService.getFollowing(userDetails.getUser());
+
+        return ResponseEntity.ok(RootResponseDto.builder()
+            .code("200")
+            .message("팔로잉 조회 성공")
+            .data(responseDto)
+            .build()
+        );
+    }
+
+    @GetMapping("/follower")
+    public ResponseEntity<?> getFollower(
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+
+        List<FollowerResponseDto> responseDto = followService.getFollower(userDetails.getUser());
+
+        return ResponseEntity.ok(RootResponseDto.builder()
+            .code("200")
+            .message("팔로워 조회 성공")
             .data(responseDto)
             .build()
         );
