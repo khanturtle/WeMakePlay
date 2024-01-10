624c5c025eda9d171ca0463dceb810440eae45b4
k <kkkjjj320320@naver.com>
2024-01-10 21:37:55 +0900

    docs: 문서 추가

--- src/main/java/com/wemakeplay/wemakeplay/global/config/WebSecurityConfig.java
+++ src/main/java/com/wemakeplay/wemakeplay/global/config/WebSecurityConfig.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -67,6 +66,7 @@ public class WebSecurityConfig {
 
         httpSecurity.authorizeHttpRequests((authorizeHttpRequests) ->
             authorizeHttpRequests
+                .requestMatchers("/error").permitAll()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/api/users/**").permitAll()
