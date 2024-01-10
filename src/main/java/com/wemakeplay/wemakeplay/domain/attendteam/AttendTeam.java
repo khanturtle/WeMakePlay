745321685896c98a4ca17dd949a28a78b97e788e
k <kkkjjj320320@naver.com>
2024-01-10 11:22:49 +0900

    docs: 문서 수정 Setter추가

--- src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
@@ -6,8 +6,10 @@ import com.wemakeplay.wemakeplay.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor
 public class AttendTeam {
