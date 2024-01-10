dfeba0c123ad963a6caec1349e81c71d4b573736
k <kkkjjj320320@naver.com>
2024-01-10 11:23:56 +0900

    docs: 추가

--- src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeamRepository.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeamRepository.java
@@ -1,8 +1,11 @@
 package com.wemakeplay.wemakeplay.domain.attendteam;
 
+import com.wemakeplay.wemakeplay.domain.attendboard.Participation;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendTeamRepository extends JpaRepository<AttendTeam, Long> {
     List<AttendTeam> findByTeamId(Long teamId);
+
+    List<AttendTeam> findByTeamIdAndParticipation(Long teamId, Participation participation);
 }
