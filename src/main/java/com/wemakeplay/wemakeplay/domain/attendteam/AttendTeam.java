0ac7bcbfe235e3b8be84217f73d8fdf77b4e434f
k <kkkjjj320320@naver.com>
2024-01-10 21:48:13 +0900

    docs: 문서 수정

--- src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
@@ -1,41 +1,43 @@
 package com.wemakeplay.wemakeplay.domain.attendteam;
 
 import static com.wemakeplay.wemakeplay.domain.attendteam.Participation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.wemakeplay.wemakeplay.domain.team.entity.Team;
 import com.wemakeplay.wemakeplay.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @NoArgsConstructor
 public class AttendTeam {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
+    @JsonIgnore
     private Team team;
 
     @Enumerated(EnumType.STRING)
     private Participation participation;
 
+    @Builder
     public AttendTeam(Team team, User user, Participation participation){
         this.user = user;
         this.team = team;
         this.participation = participation;
     }
 
-    public AttendTeam(Team team, User user, com.wemakeplay.wemakeplay.domain.attendboard.Participation participation) {
-    }
+
 
     public void allowAttend(){
         this.participation = attend;
