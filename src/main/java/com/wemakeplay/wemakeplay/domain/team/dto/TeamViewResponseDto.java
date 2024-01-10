b3604d59234d9071bf2cfcb16066f3818d7f525a
k <kkkjjj320320@naver.com>
2024-01-10 21:40:20 +0900

    feat: 팀 참여자 리스트 추가

---
+++ src/main/java/com/wemakeplay/wemakeplay/domain/team/dto/TeamViewResponseDto.java
@@ -0,0 +1,23 @@
+package com.wemakeplay.wemakeplay.domain.team.dto;
+
+import com.wemakeplay.wemakeplay.domain.team.entity.Team;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TeamViewResponseDto {
+    private Long id;
+    private String teamName;
+    private String teamIntro;
+    private int teamAttendPersonnel;
+    private int teamPersonnel;
+
+    public TeamViewResponseDto(Team team){
+        this.id = team.getTeamOwner().getId();
+        this.teamName = team.getTeamName();
+        this.teamIntro = team.getTeamIntro();
+        this.teamPersonnel = team.getTeamPersonnel();
+        this.teamAttendPersonnel = team.getTeamAttendPersonnel();
+    }
+}
