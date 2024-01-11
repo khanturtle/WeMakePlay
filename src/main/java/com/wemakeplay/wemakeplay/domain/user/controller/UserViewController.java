3cf2a59c124e21d55101a6afd4e0f3083ad76733
khanturtle <chojunho0310@naver.com>
2024-01-11 11:03:22 +0900

    feat: 회원가입 프론트

--- src/main/java/com/wemakeplay/wemakeplay/domain/user/controller/UserViewController.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/user/controller/UserViewController.java
@@ -1,35 +1,44 @@
 package com.wemakeplay.wemakeplay.domain.user.controller;
 
 import com.wemakeplay.wemakeplay.domain.user.dto.request.SignupRequestDto;
-import com.wemakeplay.wemakeplay.domain.user.dto.response.SignupResponseDto;
 import com.wemakeplay.wemakeplay.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class UserViewController {
     private final UserService userService;
-
+    //회원가입
     @GetMapping("/signup")
     public String showSignupForm() {
         return "signup";
     }
+    //성공 시 로그인 페이지로
     @PostMapping("/signup")
     public String signUp(@ModelAttribute("signupRequestDto") SignupRequestDto requestDto) {
         userService.signUp(requestDto);
-        return "redirect:main";
+        return "redirect:login";
+    }
+    //로그인 페이지
+    @GetMapping("/login")
+    public String showLoginForm(){
+        return "login";
+    }
+    //성공 시 메인 페이지로
+    @PostMapping("/login")
+    public String login(){
+        return "redirect:mainPage";
+    }
+    //메인 페이지
+    @GetMapping("/mainPage")
+    public String showMainPage(){
+        return "mainPage";
     }
 
-    @GetMapping("/main")
-    public String mainPage(){
-        return "signup_success";
-    }
 }
