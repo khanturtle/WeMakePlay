45923a74d3d4e3c158a8794cfd192468339c0a84
k <kkkjjj320320@naver.com>
2024-01-21 11:21:10 +0900

    feat: 페이지 단위 구현

--- src/main/java/com/wemakeplay/wemakeplay/domain/team/controller/TeamViewController.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/team/controller/TeamViewController.java
@@ -9,6 +9,8 @@ import com.wemakeplay.wemakeplay.global.security.UserDetailsImpl;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -47,9 +50,15 @@ public class TeamViewController {
     }
     //팀 전체 조회
     @GetMapping("/teamAttend")
-    public String getAllTeams(Model model){
-        List<TeamViewResponseDto> teamList = teamService.getAllTeams();
-        model.addAttribute("teamList", teamList);
+    public String getAllTeams(
+        Model model,
+        @RequestParam(defaultValue = "0") int page){
+        int pageSize = 10;
+        PageRequest pageable = PageRequest.of(page, pageSize);
+        Page<TeamViewResponseDto> teamPage = teamService.getAllTeams(pageable);
+        model.addAttribute("teamList", teamPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", teamPage.getTotalPages());
         return "teamAttend";
     }
     //수정
