995bac3fc5e720811cfad367f590068622ac13de
k <kkkjjj320320@naver.com>
2024-02-04 07:16:00 +0900

    fix: 팀 강퇴 기능 오류 수정

--- src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
+++ src/main/java/com/wemakeplay/wemakeplay/domain/attendteam/AttendTeam.java
@@ -8,8 +8,10 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor
 public class AttendTeam {
